0c1daaf8e70bf3fa7272a6b36bcdc62134a0289d
L-Karimi <luciahkei101@gmail.com>
2023-05-30 11:20:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+Lets do this        </a>
       </header>
     </div>
   );
