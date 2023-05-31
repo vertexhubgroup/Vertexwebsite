cccc3aa32f3f068ff1c491da76901721cd9edab5
L-Karimi <luciahkei101@gmail.com>
2023-05-31 12:28:26 +0300

    added services page and styled

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,12 +2,32 @@ import React from 'react';
 import "./pages.css"
 const Services = () => {
 	
-return (
-	<>
-		<p  className='services'>
-			Welcome to Vertexhub Group</p>
-	</>
-)
-};
-
+	return (
+		<>
+		<div className="content">
+		  <div>
+			<div className="about">
+			  <h1> Our Services</h1>
+			</div>
+	
+			<div className="image3"></div>
+			<br />
+			<br />
+			<br />
+	
+			<div className="paragraph">
+				<h5>A constantly evolving portfolio to meet the ever-changing needs of our clients.</h5>
+			  <p>
+			  We focus on three ‘playing fields’ dedicated to the digitalization of key management areas at the
+ core of businesses: Customer First, Intelligent Industry, and Enterprise Management.
+			  </p>
+			</div>
+			<div className="image2">
+			  <p></p>
+			</div>
+		  </div>
+		  </div>
+		</>
+	  );
+	};
 export default Services;
