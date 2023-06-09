d6957e99a92f3cba0149e4f4ddb753d24c8440ae
L-Karimi <luciahkei101@gmail.com>
2023-06-09 11:13:30 +0300

    Styled services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,119 +1,175 @@
 import React, { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import consultancy from "./../consultancy.png";
 import support from "./../support.jpg";
 import cloud from "./../cloud-computing.png";
 import services from "./../male-services-support-icon.png";
 import dev from "./../software dev.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Services = ({ imageSrc, caption }) => {
-  const [toggle, setToggle] = useState(true);
+  const [selectedColumn, setSelectedColumn] = useState(null);
+
+  const handleColumnClick = (column) => {
+    setSelectedColumn(column);
+  };
 
   return (
-    <>
-      <div className="content">
-        <div>
-          <div className="about">
-            <h1> Our Services</h1>
+    <div className="content">
+      <div className="about">
+        <h1>Our Services</h1>
+      </div>
+      <div className="image2"></div>
+
+      <br />
+      <br />
+
+      <div className="paragraph">
+        <h5>
+          A constantly evolving portfolio to meet the ever-changing needs of our
+          clients.
+        </h5>
+        <p>
+          We focus on three ‘playing fields’ dedicated to the digitalization of
+          key management areas at the core of businesses: Customer First,
+          Intelligent Industry, and Enterprise Management.
+        </p>
+      </div>
+
+      <div className="secondsection">
+        <h2>Explore our services</h2>
+
+        <div className="con">
+          <div
+            onClick={() => handleColumnClick("consultancy")}
+            className={`column ${
+              selectedColumn === "consultancy" ? "active" : ""
+            }`}
+          >
+            <figure>
+              <img src={consultancy} alt="Consultancy" />
+              <figcaption>consultancy</figcaption>
+            </figure>
           </div>
 
-          <div className="image2"></div>
-
-          <br />
-          <br />
-
-          <div className="paragraph">
-            <h5>
-              A constantly evolving portfolio to meet the ever-changing needs of
-              our clients.
-            </h5>
-            <p>
-              We focus on three ‘playing fields’ dedicated to the digitalization
-              of key management areas at the core of businesses: Customer First,
-              Intelligent Industry, and Enterprise Management.
-            </p>
+          <div
+            onClick={() => handleColumnClick("dev")}
+            className={`column ${selectedColumn === "dev" ? "active" : ""}`}
+          >
+            <figure>
+              <img src={dev} alt="Software Development" />
+              <figcaption>software development</figcaption>
+            </figure>
           </div>
 
-          <div className="secondsection">
-            <h2>Explore our services</h2>
+          <div
+            onClick={() => handleColumnClick("cloud")}
+            className={`column ${selectedColumn === "cloud" ? "active" : ""}`}
+          >
+            <figure>
+              <img src={cloud} alt="Cloud Computing" />
+              <figcaption>Cloud computing</figcaption>
+            </figure>
+          </div>
 
-            <div className="con">
-              <a onClick={() => setToggle(!toggle)}>
-                <div className="columnconsultancy">
-                  <figure>
-                    <img src={consultancy} alt="Consultancy" />
-                    <figcaption>consultancy</figcaption>
-                  </figure>
-                </div>
-              </a>
-              <a href="">
-                <div className="columndev">
-                  <figure>
-                    <img src={dev} alt="Software Development" />
-                    <figcaption>software development</figcaption>
-                  </figure>
-                </div>
-              </a>
+          <div
+            onClick={() => handleColumnClick("services")}
+            className={`column ${
+              selectedColumn === "services" ? "active" : ""
+            }`}
+          >
+            <figure>
+              <img src={services} alt="Services" />
+              <figcaption>Services</figcaption>
+            </figure>
+          </div>
 
-              <a href="">
-                <div className="columncloud">
-                  <figure>
-                    <img src={cloud} alt="Cloud Computing" />
-                    <figcaption>Cloud computing</figcaption>
-                  </figure>
-                </div>
-              </a>
-
-              <a href="">
-                <div className="columnservices">
-                  <figure>
-                    <img src={services} alt="Services" />
-                    <figcaption>Services</figcaption>
-                  </figure>
-                </div>
-              </a>
-
-              <a href="">
-                <div className="columnsupport">
-                  <figure>
-                    <img src={support} alt="Support" />
-                    <figcaption>support</figcaption>
-                  </figure>
-                </div>
-              </a>
-            </div>
-
-            {toggle && (
-              <ul className="list-group">
-                <h5>ICT Consultancy</h5>
-                <p>
-                  We consult for high-end technology implementation at clients’
-                  site or implementation of new technology according to the
-                  clients’ requirement or upgrading, enhancing the existing
-                  facilities in the clients end with the new technology
-                  integrated with the existing one.
-                </p>
-                <p>
-                  VERTEX HUB offers a full range of consulting services to help
-                  analyse your business requirements for effective
-                  implementation __of solutions. Our consulting services cover:
-                </p>
-                <li className="list-group-item" >  <FontAwesomeIcon icon={faSquareCheck} className="fa-square-check"  bounce />Strategy Planning</li>
-                <li className="list-group-item"> <FontAwesomeIcon icon={faSquareCheck} className="fa-square-check"  bounce /> Assessment</li>
-                <li className="list-group-item"> <FontAwesomeIcon icon={faSquareCheck} className="fa-square-check"  bounce /> Procurement</li>
-                <li className="list-group-item"><FontAwesomeIcon icon={faSquareCheck} className="fa-square-check"  bounce />  Re-engineering Solutions</li>
-                <li className="list-group-item"> <FontAwesomeIcon icon={faSquareCheck} className="fa-square-check"  bounce />Disaster Recovery</li>
-              </ul>
-            )}
+          <div
+            onClick={() => handleColumnClick("support")}
+            className={`column ${selectedColumn === "support" ? "active" : ""}`}
+          >
+            <figure>
+              <img src={support} alt="Support" />
+              <figcaption>support</figcaption>
+            </figure>
           </div>
         </div>
+
+        <div className="column-content">
+          {selectedColumn && (
+            <ul className="list-group">
+              {selectedColumn === "consultancy" && (
+                <>
+                  <h5>ICT Consultancy</h5>
+                  <p>
+                    We consult for high-end technology implementation at
+                    clients’ site or implementation of new technology according
+                    to the clients’ requirement or upgrading, enhancing the
+                    existing facilities in the clients end with the new
+                    technology integrated with the existing one.
+                  </p>
+                  <p>
+                    VERTEX HUB offers a full range of consulting services to
+                    help analyse your business requirements for effective
+                    implementation of solutions. Our consulting services cover:
+                  </p>
+                  <li className="list-group-item">
+                    <FontAwesomeIcon
+                      icon={faSquareCheck}
+                      className="fa-square-check"
+                      bounce
+                    />
+                    Strategy Planning
+                  </li>
+                  <li className="list-group-item">
+                    <FontAwesomeIcon
+                      icon={faSquareCheck}
+                      className="fa-square-check"
+                      bounce
+                    />
+                    Assessment
+                  </li>
+                  <li className="list-group-item">
+                    <FontAwesomeIcon
+                      icon={faSquareCheck}
+                      className="fa-square-check"
+                      bounce
+                    />
+                    Procurement
+                  </li>
+                  <li className="list-group-item">
+                    <FontAwesomeIcon
+                      icon={faSquareCheck}
+                      className="fa-square-check"
+                      bounce
+                    />
+                    Re-engineering Solutions
+                  </li>
+                  <li className="list-group-item">
+                    <FontAwesomeIcon
+                      icon={faSquareCheck}
+                      className="fa-square-check"
+                      bounce
+                    />
+                    Disaster Recovery
+                  </li>
+                </>
+              )}
+
+              {selectedColumn === "dev" && <p>we are here to work</p>}
+
+              {selectedColumn === "cloud" && <p>we are here to work</p>}
+
+              {selectedColumn === "services" && <p>we are here to work</p>}
+
+              {selectedColumn === "support" && <p>we are here to work</p>}
+            </ul>
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
